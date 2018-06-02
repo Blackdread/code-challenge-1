92e9fb52d283a9293181d9bf8513600c4b557eac
yoann <yoann@alluresys.com>
2018-06-02 10:25:11 +0800

    Initial application generated by JHipster-4.14.4

---
+++ src/main/java/org/blackdread/test/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package org.blackdread.test.repository;
---
+++ src/main/java/org/blackdread/test/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.blackdread.test.web.rest.vm;
